68a80a7de9af0511d73b4856be5e3f845ae74eb2
Arife Ljutviovska <38080549+Arifee@users.noreply.github.com>
2019-11-12 13:25:33 +0100

    Add files via upload

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd /home/ec2-user/server
+service httpd start
 sudo /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.222.b10-0.amzn2.0.1.x86_64/jre/bin/java -jar -Dserver.port=80 \
     *.jar > /dev/null 2> /dev/null < /dev/null &
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sudo killall java
+service httpd stop
 exit 0
